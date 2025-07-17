39b91beaf67ec3611fee2188e695c9ccda8f28a4
heliannuuthus <118797425+heliannuuthus@users.noreply.github.com>
2025-07-17 07:12:56 +0000

    implement sm3 ObjectIdentifier

--- sm3/src/lib.rs
+++ sm3/src/lib.rs
@@ -20,5 +20,6 @@ pub use block_api::Sm3Core;
 digest::buffer_fixed!(
     /// ShangMi 3 (SM3) hasher.
     pub struct Sm3(block_api::Sm3Core);
+    oid: "2.16.840.1.101.3.4.2.1";
     impl: FixedHashTraits;
 );
